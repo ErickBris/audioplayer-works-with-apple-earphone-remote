3e2194e8559fbd8ce7d721fc8405fa225ab960bd
ErickBris <erick1saradmon@gmail.com>
2022-05-02 17:27:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# audioplayer-works-with-apple-earphone-remote
+ audioplayer-works-with-apple-earphone-remote
---
+++ Xcode Files/Audioplayer/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  Audioplayer
+//
+//  Created by Paolo Rossignoli on 11/02/14.
+//  Copyright (c) 2014 Paolo Rossignoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
